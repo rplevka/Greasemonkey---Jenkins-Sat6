9ab6f0f7d8c7b3ec511ffacc9c54aa4d95b56a81
Roman Plevka <rplevka@redhat.com>
2016-12-20 11:57:56 +0100

    proper version handling

---
+++ test-results-analyzer.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        jenkins_test_results_analyzer_redesign
+// @namespace   jenkins-ci
+// @include     https://*jenkins*.redhat.com/*/test_results_analyzer/
+// @version     1.2
+// @grant       none
+// @updateurl   https://raw.githubusercontent.com/rplevka/Greasemonkey---Jenkins-Sat6/master/test-results-analyzer.meta.js
+// @downloadurl https://raw.githubusercontent.com/rplevka/Greasemonkey---Jenkins-Sat6/master/test-results-analyzer.user.js
+// @author      roman plevka (rplevka@redhat.com)
+// ==/UserScript==
--- test-results-analyzer.user.js
+++ test-results-analyzer.user.js
@@ -4,8 +4,8 @@
 // @include     https://*jenkins*.redhat.com/*/test_results_analyzer/
 // @version     1.2
 // @grant       none
-// @updateurl   https://raw.githubusercontent.com/rplevka/Greasemonkey---Jenkins-Sat6/master/test-results-analyzer.js
-// @downloadurl https://raw.githubusercontent.com/rplevka/Greasemonkey---Jenkins-Sat6/master/test-results-analyzer.js
+// @updateurl   https://raw.githubusercontent.com/rplevka/Greasemonkey---Jenkins-Sat6/master/test-results-analyzer.meta.js
+// @downloadurl https://raw.githubusercontent.com/rplevka/Greasemonkey---Jenkins-Sat6/master/test-results-analyzer.user.js
 // @author      roman plevka (rplevka@redhat.com)
 // ==/UserScript==
 
